1f37fb347cad7a1195cd0d677852782bcb342e50
DanteBoom <salamandrboom@gmail.com>
2018-09-23 23:18:20 +0300

    love-triangle: finished task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,33 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var inputArr = preferences;
+  var arrLength = inputArr.length;
+
+  var reindexedArr = [];
+  inputArr.forEach(function(value, i){
+    reindexedArr[i+1] = value;
+  });
+
+  var trianglesCounter = 0;
+  var i;
+  for(i = 1; i <= arrLength; i++){
+    if(reindexedArr[i] != 
+    reindexedArr[reindexedArr[i]])
+    {
+      if(reindexedArr[reindexedArr[i]] != 
+      reindexedArr[reindexedArr[reindexedArr[i]]])
+      {
+        if((reindexedArr[reindexedArr[reindexedArr[i]]] != reindexedArr[reindexedArr[reindexedArr[reindexedArr[i]]]]) && 
+        (reindexedArr[reindexedArr[reindexedArr[reindexedArr[i]]]] == reindexedArr[i]))
+        {
+          trianglesCounter = trianglesCounter + 1;
+          delete reindexedArr[reindexedArr[reindexedArr[i]]];
+          delete reindexedArr[reindexedArr[i]];
+          delete reindexedArr[i];
+        }
+      }
+    }
+  }  
+  return trianglesCounter;
 };
